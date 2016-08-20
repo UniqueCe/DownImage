cdf4715bb9c2842f8cb48483a58d8519ffaf5655
UniqueCe <15011529145@163.com>
2016-08-20 22:18:46 +0800

    新建仿SDWebImage

---
+++ 仿SDWebImage/仿SDWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿SDWebImage
+//
+//  Created by 刘培策 on 16/8/20.
+//  Copyright © 2016年 刘培策. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 仿SDWebImage/仿SDWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  仿SDWebImage
+//
+//  Created by 刘培策 on 16/8/20.
+//  Copyright © 2016年 刘培策. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
